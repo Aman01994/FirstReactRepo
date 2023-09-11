1ba4d2dbe2da4b4ec14db6f0d6b889af2e75a56d
Aman01994 <aman.kohli.2009@gmail.com>
2023-09-12 00:57:20 +0530

    v1

---
+++ src/Components/Comp1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Comp1 extends Component{
+    // 1. Properties
+
+
+    // 2. Constructor 
+
+
+    // 3. Methods 
+}
+
+export default Comp1
