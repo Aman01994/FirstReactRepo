12c27163922896482a78078b3dd58cda85b0c1cc
Aman01994 <aman.kohli.2009@gmail.com>
2023-09-13 02:30:12 +0530

    Components linked removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 
-import Comp1 from "./Components/Comp1";
-import { Comp2 } from "./Components/Comp2";
-
 function App() {
   return (
     <>
-    <Comp1 firstName="Amandeep Singh" lastName="Kohli" />
-    <Comp2/>
+    
     </>  
 );
 }
