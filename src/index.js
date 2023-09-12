ecbcbb22d98feb828f7017a23e5ed48b5b5d0c00
Aman01994 <aman.kohli.2009@gmail.com>
2023-09-13 03:19:51 +0530

    Props usage in function Components and Class Components done Continue Tomorrow

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-  
+// Funtion Component 
+function A(props){
+  let name = "Jatin"
+  return<>
+  <h1>{name}, {props.surname}</h1>;
+  <h2><B surname='kohli'/></h2>
   </>
-);
+}
+
+// Class Component
+// class child extends parent {}  
+class B extends Component{
+  // Properties 
+  name = "MANDEEP"
+
+  // Constructor 
+
+
+  // Method 
+  render(){
+    return (
+      <span>  {this.name}, {this.props.surname}</span>
+    )
+  
+  }
+}
+let surnameA = 'DEEP Ssingh'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<A surname={surnameA}/>);
 
 
