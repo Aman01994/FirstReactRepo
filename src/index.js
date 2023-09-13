a0f4b7c593760f782211649c048c375d453a1d5c
Aman01994 <aman.kohli.2009@gmail.com>
2023-09-13 14:35:17 +0530

    Done for today

--- src/index.js
+++ src/index.js
@@ -7,30 +7,40 @@ import ReactDOM from 'react-dom/client';
 class B extends Component{
   // properties
   name = 'Amandeep'
-  
+  age;
+  address;
+  height;
+  hairColor;
   //Constructure
-  
+  constructor(){
+    super()
+    this.age = 45
+    
+  }
 
 
   //Methods
   render(){
-    return <h1>Hello my name is {this.name} {this.props.surname}</h1>
+  
+    return <h1>Hello my name is {this.name}, my age is {this.age}  </h1>
   }
+  
 }
 
 // Function Componenets 
 
-function A(props){
-  let firstName = "Surjit"
+// function A(props){
+//   // let firstName = "Surjit"
   
-  return <>
-  <h1>hi my name is {firstName} {props.children} </h1>
-  </>
-}
+//   return <>
+//   <h1>hi my name is {props.firstNames} {props.lastName} </h1>
+
+//   </>
+// }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render( <><B surname='Kohli'/> <A>Kohli</A> </>);
+root.render( <> <B/> </>);
 
 
